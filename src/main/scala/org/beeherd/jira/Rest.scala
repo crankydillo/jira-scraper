2de88069c892dbde56f6a23d7368f483487460ea
samuel <crankydillo@gmail.com>
2013-04-08 04:34:07 -0500

    Forgot the REST file..

---
+++ src/main/scala/org/beeherd/jira/Rest.scala
@@ -0,0 +1,65 @@
+package org.beeherd.jira
+
+import java.text.SimpleDateFormat
+
+import org.joda.time.DateTime
+import org.beeherd.client.http.HttpClient
+import net.liftweb.json.DefaultFormats
+import net.liftweb.json.Serialization.read
+import net.liftweb.json.ext.JodaTimeSerializers
+
+object JsonResults {
+  case class SearchResult(maxResults: Int, issues: List[Issue])
+  case class WorkLogResult(worklogs: List[WorkLog])
+}
+
+trait RestResource {
+  implicit val formats = new DefaultFormats {
+    override def dateFormatter = new SimpleDateFormat(
+      "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+  } ++ JodaTimeSerializers.all
+}
+
+/**
+ * Access Jira's ../search resource.
+ */
+class JiraSearcher(
+  client: HttpClient
+  , jiraUrlBase: String // string?? base URL for jira REST api
+) extends RestResource {
+  import JsonResults.SearchResult
+
+  private val searchResourceUrl = jiraUrlBase + "/search"
+
+  def issues(jql: String, includeSubtasks: Boolean = false): List[Issue] =
+    read[SearchResult](
+      client.get(searchResourceUrl, Map("jql" -> jql)).content.get.toString
+    ).issues
+}
+
+class JiraIssue(
+  client: HttpClient
+  , jiraUrlBase: String // string?? base URL for jira REST api
+) extends RestResource {
+
+  private val baseUrl = jiraUrlBase + "/issue"
+
+  def issue(id: String): Issue = 
+    read[Issue](
+      client.get(baseUrl + "/" + id).content.get.toString
+    )
+}
+
+/**
+ * Access JIRA's ../worklog resource.
+ */
+class JiraWorklog(
+  client: HttpClient
+) extends RestResource {
+  import JsonResults.WorkLogResult
+
+  def worklogs(issue: Issue): List[WorkLog] =
+    read[WorkLogResult](
+      client.get(issue.url + "/worklog").content.get.toString
+    ).worklogs
+}
